28a794075b85def337a743cd329448a553f26ee2
Eros Chagas <eroschagas@hotmail.com>
2022-04-11 18:09:02 -0300

    terminando a aula de testes

---
+++ src/components/TextInput/textinput.spec.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { TextInput } from ".";
+
+describe("<TextInput />", () => {
+  it("should have a value of searchValue", () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} searchValue={"testando"} />);
+    const input = screen.getByPlaceholderText(/type your search/i);
+    expect(input.value).toBe("testando");
+  });
+  it("should call handleChange function at each key press", () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} />);
+    const input = screen.getByPlaceholderText(/type your search/i);
+    const value = "the value";
+    userEvent.type(input, value);
+    expect(input.value).toBe(value);
+    expect(fn).toHaveBeenCalledTimes(value.length);
+  });
+});
